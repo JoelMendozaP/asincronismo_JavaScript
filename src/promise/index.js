1b3ed0dbca6627537eb9b8847360353c11b3f07e
JoelM <joel@Laptop.com>
2020-02-18 20:30:59 -0400

    Utilizando un api con promesas

---
+++ src/promise/index.js
@@ -0,0 +1,40 @@
+const somethingWillHappen = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve('Hey!');
+        }else{
+            reject('Whooops!');
+        }
+    });
+};
+
+somethingWillHappen()
+ .then(response => console.log(response))
+ .catch(err => console.error(err))
+
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if(true){
+            setTimeout(() => {
+                resolve('True');
+            }, 2000)
+        }else{
+            const error = new Error('Wooops!');
+            reject(error)
+        }
+    })
+}
+somethingWillHappen2()
+    .then(response => console.log(response))
+    .catch(err => console.log(err));
+
+//varias promesas al mismo tiempo
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+    .then(response => {
+        console.log('Array of result ', response);
+    })    
+    .catch(err => {
+        console.error(err);
+    })
